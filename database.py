c0dc0c0b2f0fb0c52d10359d01c23a554138afc9
Shafayat Saad <dev.saad30@gmail.com>
2026-08-10 01:14:24 +0600

    Update database.py

--- database.py
+++ database.py
@@ -48,17 +48,18 @@ def initialize_database():
     ).fetchone()[0]
 
     if count == 0:
-        connection.executemany(
-            """
-            INSERT INTO tasks (title, done)
-            VALUES (%s, %s)
-            """,
-            [
-                ("Buy milk", False),
-                ("Walk the dog", True),
-                ("Finish CRUD assignment", False),
-            ],
-        )
+        with connection.cursor() as cursor:
+            cursor.executemany(
+                """
+                INSERT INTO tasks (title, done)
+                VALUES (%s, %s)
+                """,
+                [
+                    ("Buy milk", False),
+                    ("Walk the dog", True),
+                    ("Finish CRUD assignment", False),
+                ],
+            )
 
         connection.commit()
 
